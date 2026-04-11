1b9b010f1851355f38844f9afdaddabeec01dabd
longjunfan <2933986273@qq.com>
2026-04-11 20:07:48 +0800

    Delete mass streamfunction directory

--- mass streamfunction/curlz_atmos.m
+++
@@ -1,9 +0,0 @@
-function curlz=curlz_atmos(longitude, latitude, u, v)
-    R=6371393.0; % earth's radius
-    dx=dx_atmos(longitude, latitude);
-    dy=dy_atmos(latitude);
-    [~, du]=gradient(u);
-    [dv, ~]=gradient(v);
-    curlz=dv./dx-du./dy+u.*tan(latitude.*pi./180)./R;
-    curlz(abs(latitude)==90)=NaN;
-end
--- mass streamfunction/grad_atmos.m
+++
@@ -1,7 +0,0 @@
-function [grad_x, grad_y]=grad_atmos(longitude, latitude, H)
-    dx=dx_atmos(longitude, latitude);
-    dy=dy_atmos(latitude);
-    [grad_x, grad_y]=gradient(H);
-    grad_x=grad_x./dx;
-    grad_y=grad_y./dy;
-end
